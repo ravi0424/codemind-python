c2b067c7ad90a0a4fb8b91eb094118996f2b7042
ravi0424 <102217635+ravi0424@users.noreply.github.com>
2022-05-01 15:31:22 +0530

    def fib(n):     a=0     b=1     c=0     while True:         c=a+b         a=b         b=c         if c==n:             return True         if c>n:             return False a=int(input()) if fib(a):     print("True") else:     print("False")

---
+++ Fibonacci_or_not.py
@@ -0,0 +1,18 @@
+def fib(n):
+    a=0
+    b=1
+    c=0
+    while True:
+        c=a+b
+        a=b
+        b=c
+        if c==n:
+            return True
+        if c>n:
+            return False
+a=int(input())
+if fib(a):
+    print("True")
+else:
+    print("False")
+        
