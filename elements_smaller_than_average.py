c26f2951089596c21d9bd552e18beafa8627e4f5
ravi0424 <102217635+ravi0424@users.noreply.github.com>
2022-05-15 14:04:19 +0530

    def count(a): 	b=0 	c=0 	for i in a: 		b+=i 	c=b/len(a) 	return c a=int(input()) b=list(map(int,input().split())) c=count(b) d=0 for i in b: 		if i<=c: 			d+=1 print(d)

---
+++ elements_smaller_than_average.py
@@ -0,0 +1,16 @@
+def count(a):
+	b=0
+	c=0
+	for i in a:
+		b+=i
+	c=b/len(a)
+	return c
+a=int(input())
+b=list(map(int,input().split()))
+c=count(b)
+d=0
+for i in b:
+		if i<=c:
+			d+=1
+print(d)
+		
