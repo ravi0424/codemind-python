8bad7d004c7be888d7205ffeb482342612ea2c7b
ravi0424 <102217635+ravi0424@users.noreply.github.com>
2022-05-18 08:32:44 +0530

    def find(a): 	b=[] 	c,d=0,0 	for i in a: 		c=i 		d=a.count(i) 		if d==c: 			if i in b: 				continue 			else: 				b.append(i) 	return b a=int(input()) b=list(map(int,input().split())) c=find(b) if len(c)!=0: 	print(*c) else: 	print(-1)

---
+++ Elements_same_as_count.py
@@ -0,0 +1,19 @@
+def find(a):
+	b=[]
+	c,d=0,0
+	for i in a:
+		c=i
+		d=a.count(i)
+		if d==c:
+			if i in b:
+				continue
+			else:
+				b.append(i)
+	return b
+a=int(input())
+b=list(map(int,input().split()))
+c=find(b)
+if len(c)!=0:
+	print(*c)
+else:
+	print(-1)
